572a3cf59aa28866e90522f7629ee9b20c0a9b63
Gregory <gigirashvili@gmail.com>
2021-04-01 11:57:09 +0300

    Test DZ1

---
+++ src/main/java/ru/geekbrains/org/webDriverWait.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.org;
+
+public class webDriverWait {
+}
--- src/test/java/GB/TestDZTest.java
+++ src/test/java/GB/TestDZTest.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.org;
+package GB;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
