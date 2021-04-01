697d4fd1b85ad0a5df947e74bdf20900874b0227
Gregory <gigirashvili@gmail.com>
2021-04-01 11:31:39 +0300

    Test DZ1

---
+++ src/test/java/ru/geekbrains/org/TestDZTest.java
@@ -0,0 +1,74 @@
+package ru.geekbrains.org;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+class TestDZTest {
+    private final String LOGIN_PAGE_URL = "https://crm.geekbrains.space/user/login";
+    private final String STUDENT_LOGIN = "Applanatest";
+    private final String STUDENT_PASSWORD = "Student2020!";
+    private WebDriver driver;
+
+
+
+
+    @Test
+    public void CreateContactPerson() {
+//Нвести мыш на вкладку "Контрагенты"
+        By btnLocator = By.xpath("xpath=//*[@id=\"main-menu\"]/ul/li[1]/a/span");
+        Actions actions = new Actions(driver);
+        WebElement menuBtn = driver.findElement(btnLocator);
+        actions.moveToElement(menuBtn);
+
+        //Открыть меню "Контактные лица"
+        WebElement SubMenu = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[1]/ul/li[4]/a/span"));
+        actions.moveToElement(SubMenu);
+        actions.click().build().perform();
+
+        //Нажать на кнопку "Создать контактное лицо"
+        driver.findElement(By.xpath("xpath=//a[@title='Создать контактное лицо']"));
+
+        //Ожидаем перехода на страницу "Создать контактное лицо"
+        WebDriverWait waitFiveSeconds = new WebDriverWait(driver, 5);
+        waitFiveSeconds.until(ExpectedConditions.urlContains("/create"));
+
+        //Заполнить поле "Фамилия"
+        driver.findElement(By.xpath("xpath=//div/div/div[2]/input"));
+
+        //Заполнить поле "Имя"
+        driver.findElement(By.xpath("xpath=//div/div[2]/div[2]/input"));
+
+        //Выюираю свою Организацию
+        driver.findElement(By.xpath("xpath=//div[2]/div/div/a/span"));
+
+        //Заполнить поле "Должность"
+        driver.findElement(By.xpath("xpath=//div[2]/div[2]/div[2]/input"));
+
+        //Нажать "Сохранить и закрыть"
+        driver.findElement(By.cssSelector("button[class=btn btn-success action-button"));
+
+//Добавление Assert
+        WebElement element = driver.findElement(By.xpath("//div[@class='alert alert-success"));
+        Assertions.assertTrue(element.isDisplayed());
+        List<WebElement> message = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("//div[@class='alert alert-success fade in top message']")));
+        Assertions.assertTrue(message.contains("Контактноье лицо сохранено"));
+    }
+
+}
